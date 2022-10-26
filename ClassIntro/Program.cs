6bed829c499c44b36e424afef476cfda3378bd9e
KML-SNR <KML-SNR@DESKTOP-65FA2CK>
2022-10-26 17:10:19 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.KursunEgitmeni = "Engin Demirog";
+            kurs1.İzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.KursunEgitmeni = "Kerem Varış";
+            kurs2.İzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.KursunEgitmeni = "Berkay Bilgin";
+            kurs3.İzlenmeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.KursunEgitmeni = "Murat Kurtboğan";
+            kurs4.İzlenmeOrani = 100;
+
+
+            //Console.WriteLine(kurs1.KursAdi + " " + kurs1.KursunEgitmeni);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3,kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.KursunEgitmeni);
+            }
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursunEgitmeni { get; set; }
+        public int İzlenmeOrani { get; set; }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım gelitirici kursu";
+            string kurs2 = "Probramlamaya başlangıç için kurs";
+            string kurs3 = "Java";
+            
+            //array - liste 
+
+            string[] kurslar = new string[] { "Yazılım gelitirici kursu", "Probramlamaya başlangıç için kurs", "Java", "Python","C#"};
+
+
+            for (int i = 0  ; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("for bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
